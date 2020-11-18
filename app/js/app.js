9f33e4ff13856a339b5e601ee5ef27fd82bdffcd
jordonss <nemezg@gmail.com>
2020-11-18 11:42:45 +0200

    featured section

--- app/js/app.js
+++ app/js/app.js
@@ -12,4 +12,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
   $topSliderCarousel.trigger('refresh.owl.carousel');
 
+  const $homePageFeaturedCarousel = $('.featured-list.owl-carousel');
+
+  $homePageFeaturedCarousel.owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: false,
+    margin: 30,
+    items: 1,
+    autoplayHoverPause: true,
+    navContainerClass: 'featured-arrow',
+    navClass: ['featured-arrow-prev', 'featured-arrow-next'],
+    navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+    responsive: {
+      0: {
+        items: 3,
+        slideBy: 3
+      },
+      992: {
+        items: 4,
+        slideBy: 4
+      }
+    }
+  })
+
 });
